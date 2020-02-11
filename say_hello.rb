e2e7e786d5c2541c57675fa6d84cf949236191a4
Malcom Fetus <sora.vista@gmail.com>
2020-02-11 16:42:48 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,12 @@
 # Build your say_hello method here
+def say_hello(name="Kent Beck", language="Ruby Programmer")
+  puts "Hello #{language}!"
+end 
+
+def say_hello(name="Kent Beck", language="Ruby Programmer")
+  puts "Hello #{name}!"
+end
+
+def say_hello(language="Ruby Programmer")
+  puts "Hello #{language}!"
+end 
